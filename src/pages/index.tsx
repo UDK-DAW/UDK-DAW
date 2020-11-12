78b06beb952d98048ee29ed5ce58d33a4035d969
wuzhaoyi <hqwuzhaoyi@qq.com>
2020-11-12 10:13:54 +0800

    feat: 🎸 add Piano component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './index.less';
+import styles from './index.css';
 
 export default () => {
   return (
@@ -7,4 +7,4 @@ export default () => {
       <h1 className={styles.title}>Page index</h1>
     </div>
   );
-}
+};
